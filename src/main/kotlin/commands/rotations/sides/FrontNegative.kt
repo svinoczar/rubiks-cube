cf1d6fee131ae027c7d48fbe5ae158f24d8590bb
notsunshine <alexsupper75@gmail.com>
2023-06-24 02:49:04 +0300

    modify front rotation funtions

--- src/main/kotlin/commands/rotations/sides/FrontNegative.kt
+++ src/main/kotlin/commands/rotations/sides/FrontNegative.kt
@@ -21,6 +21,24 @@ class FrontNegative: Rotation() {
     }
 
     override fun execute(cube: RubiksCube): RubiksCube {
+        val tmpLeft = cube.leftSide
+        val tmpRight = cube.rightSide
+
+        cube.rightSide[0] = cube.botSide[2]
+        cube.rightSide[7] = cube.botSide[1]
+        cube.rightSide[6] = cube.botSide[0]
+
+        cube.leftSide[2] = cube.topSide[4]
+        cube.leftSide[3] = cube.topSide[5]
+        cube.leftSide[4] = cube.topSide[6]
+
+        cube.topSide[6] = tmpRight[0]
+        cube.topSide[5] = tmpRight[7]
+        cube.topSide[4] = tmpRight[6]
+
+        cube.botSide[0] = tmpLeft[2]
+        cube.botSide[1] = tmpLeft[3]
+        cube.botSide[2] = tmpLeft[4]
         cube.frontSide.forEach {
             it.position = if (it.position != 8) if ((it.position - 2) < 0) (it.position - 2) + 8 else (it.position - 2) + 0 else it.position
         }
--- src/main/kotlin/commands/rotations/sides/FrontPositive.kt
+++ src/main/kotlin/commands/rotations/sides/FrontPositive.kt
@@ -22,6 +22,25 @@ class FrontPositive: Rotation() {
     }
 
     override fun execute(cube: RubiksCube): RubiksCube {
+        val tmpLeft = cube.leftSide
+        val tmpRight = cube.rightSide
+
+        cube.rightSide[0] = cube.topSide[6]
+        cube.rightSide[7] = cube.topSide[5]
+        cube.rightSide[6] = cube.topSide[4]
+
+        cube.leftSide[2] = cube.botSide[0]
+        cube.leftSide[3] = cube.botSide[1]
+        cube.leftSide[4] = cube.botSide[2]
+
+        cube.topSide[6] = tmpLeft[4]
+        cube.topSide[5] = tmpLeft[3]
+        cube.topSide[4] = tmpLeft[2]
+
+        cube.botSide[0] = tmpRight[6]
+        cube.botSide[1] = tmpRight[7]
+        cube.botSide[2] = tmpRight[0]
+
         cube.frontSide.forEach {
             it.position = if (it.position != 8) (it.position + 2) % 8 else it.position
         }
